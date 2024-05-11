723def8cd6e85ac8081f2168fda3aa9a541f42df
Iqrashaikh20 <iqrashaikh.in22@gmail.com>
2024-05-11 14:24:01 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -1,13 +1,13 @@
- * Question Set 1 - Easy *
+ /* Question Set 1 - Easy */
 
-/ Q1: Who is the senior most employee based on job title? /
+/* Q1: Who is the senior most employee based on job title? */
 
 SELECT title, last_name, first_name 
 FROM employee
 ORDER BY levels DESC
 LIMIT 1;
 
-/ Q2: Which countries have the most Invoices? /
+/* Q2: Which countries have the most Invoices? */
 
 ORDER BY C DESC
 SELECT COUNT (*), billing_country
@@ -15,24 +15,24 @@ FROM invoice
 GROUP BY billing_country
 ORDER BY billing_country DESC;
 
-/ Q3: What are the top 3 values of the total invoice? /
+/* Q3: What are the top 3 values of the total invoice? */
 
 SELECT total
 FROM invoice
 ORDER BY total DESC
 LIMIT 3;
 
-/ Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city where we made the most money. 
+/* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city where we made the most money. 
 Write a query that returns one city that has the highest sum of invoice totals. 
-Return both the city name & sum of all invoice totals /
+Return both the city name & sum of all invoice totals */
 
 SELECT SUM (total) AS Invoice_total, billing_city
 FROM invoice
 GROUP BY billing_city
 ORDER by Invoice_total DESC;
 
-/ Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
-Write a query that returns the person who has spent the most money. /
+/* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
+Write a query that returns the person who has spent the most money. */
 
 SELECT customer.customer_id, first_name, last_name, 
 SUM (total) AS total_spending
@@ -43,10 +43,10 @@ GROUP BY customer.customer_id
 ORDER BY total_spending DESC
 LIMIT 1;
 
-* Question Set 2 - Moderate *
+/* Question Set 2 - Moderate */
 
-/ Q1: Write a query to return the email, first name, last name, & Genre of all Rock Music listeners. 
-Return your list ordered alphabetically by email starting with A. /
+/* Q1: Write a query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+Return your list ordered alphabetically by email starting with A. */
 
 SELECT DISTINCT email, first_name, last_name
 FROM customer
@@ -62,8 +62,8 @@ WHERE track_id IN (
 )
                ORDER BY email ASC;
 
-/ Q2: Let's invite the artists who have written the most rock music in our dataset. 
-Write a query that returns the Artist name and total track count of the top 10 rock bands. /
+/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
+Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
 SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
 FROM track
@@ -75,15 +75,15 @@ GROUP BY artist.artist_id
 ORDER BY number_of_songs DESC
 LIMIT 10;
 
-/ Q3: Return all the track names that have a song length longer than the average song length. 
-Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. /
+/* Q3: Return all the track names that have a song length longer than the average song length. 
+Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
 
 SELECT name, milliseconds
 FROM track
 WHERE milliseconds > (
-	                    SELECT AVG (milliseconds) AS avg_track_length
-	                    FROM track)
-ORDER BY milliseconds DESC;
+	             SELECT AVG (milliseconds) AS avg_track_length
+	             FROM track)
+      ORDER BY milliseconds DESC;
 
 
 
